995cba63e0706a3c74d658676b65c38ae433e473
ellenaua <elenasharovar@gmail.com>
2018-08-26 21:12:52 +0300

    Lesson5: Added layout

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const express = require('express')
+const exphbs  = require('express-handlebars');
 
 const app = express()
 
+app.set('views', './views')
+
+app.engine('hbs', exphbs({defaultLayout: 'main', extname: ".hbs" }));
+app.set('view engine', 'hbs');
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('index')
 })
 
 app.get('/faq', (req, res) => {
-  res.send('Frequently asked questions')
+  res.render('faq')
 })
 
 app.get('/contact', (req, res) => {
-  res.send('Contact us: support@test.com')
+  res.render('contact')
 })
 
 app.listen(3000, () => {
